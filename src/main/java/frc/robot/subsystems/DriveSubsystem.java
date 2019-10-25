bbcdf37e601e91d6c216b82f9da9b3fe5f326d51
FRC 7853 <52382182+FRC7853@users.noreply.github.com>
2019-10-24 22:13:49 -0400

    Maxed the turning speed and cleaned up the code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,29 +7,29 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.RobotMap;
+import frc.robot.commands.ManualDriveCommand;
+
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.RobotMap;
-import frc.robot.commands.ManualDriveCommand;
 
-/**
- * Add your docs here.
- */
+
 public class DriveSubsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-
+  //Defining both right motor objects
   public Victor rightMotorTop = new Victor(RobotMap.rightMotorPortTop);
   public Victor rightMotorBottom = new Victor(RobotMap.rightMotorPortBottom);
+  //Linking the right motors
   public SpeedControllerGroup right = new SpeedControllerGroup(rightMotorTop, rightMotorBottom);
 
+  //Defining both left motor objects
   public Victor leftMotorTop = new Victor(RobotMap.leftMotorPortTop);
   public Victor leftMotorBottom = new Victor(RobotMap.leftMotorPortBottom);
+  //Linking the left motors
   public SpeedControllerGroup left = new SpeedControllerGroup(leftMotorTop, leftMotorBottom);
 
-  // Differential Drive Object
+  // Differential Drive Object with the two SpeedControllerGroups
   public DifferentialDrive drive = new DifferentialDrive(left, right);
   
   // Differential Drive Constructor
@@ -39,11 +39,10 @@ public class DriveSubsystem extends Subsystem {
 
   }
   */
-
+  
+  //Drive function to use the inputs of the controller
   public void manualDrive(double move, double turn) {
-
-    drive.arcadeDrive(move, turn);
-
+    drive.arcadeDrive(move, turn * RobotMap.maxTurnSpeed);
   }
 
   @Override
